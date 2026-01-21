a09fc828b0f35b1e7eaf18ddea9926fce08be2ea
William-Madsen <williamlundgho@gmail.com>
2026-01-21 10:47:47 +0100

    COnditional Statements

---
+++ Conditional_Statements/Absolute_value.cs
@@ -0,0 +1,11 @@
+﻿int Absolute = 392;
+
+if (Absolute < 0)
+{
+    Absolute = -Absolute;
+    Console.WriteLine($"Absolute value {Absolute}");
+}
+else
+{
+    Console.WriteLine("Absolute value " + Absolute);
+}
